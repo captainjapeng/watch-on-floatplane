3a943cacada7b3d4b010c61a0752a056f0822847
Jasper Agrante <jasperneil0701@gmail.com>
2023-03-05 19:04:59 +0800

    Change phash api to support array of urls

--- worker/phash/main.ts
+++ worker/phash/main.ts
@@ -1,21 +1,27 @@
 import jimp from 'npm:jimp@0.22.7'
-import { Application, Router, helpers } from "https://deno.land/x/oak@v12.0.0/mod.ts"
+import { Application, Router } from 'https://deno.land/x/oak@v12.0.0/mod.ts'
 
 const router = new Router()
 router.get('/phash', async (context) => {
-  const query = helpers.getQuery(context)
-  if (!query.url) {
-    context.response.body = { error: `Must provide url query params`}
+  const urls = context.request.url.searchParams.getAll('url')
+
+  if (!urls.length) {
+    context.response.body = { error: 'Must provide url query params' }
     context.response.status = 400
     return
   }
 
-  const img = await jimp.read({ url: query.url })
-  context.response.body = img.hash(2)
+  const results = await Promise.all(urls.map(async (url) => {
+    if (!url) return ''
+    const img = await jimp.read({ url })
+    return img.hash(2)
+  }))
+
+  context.response.body = results
 })
 
 const app = new Application()
 app.use(router.routes())
 app.use(router.allowedMethods())
 
-await app.listen({ port: parseInt(Deno.env.get('PORT') || `8080`) })
+await app.listen({ port: parseInt(Deno.env.get('PORT') || '8080') })
